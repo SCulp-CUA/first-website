be1868a1b5f09e9ba220b534bff4106a68df07b4
SCulp-CUA <culps@cua.edu>
2025-04-15 20:06:37 -0400

    Add files via upload

---
+++ HW11/content.js
@@ -0,0 +1,29 @@
+fetch("https://picsum.photos/v2/list?page=2&limit=10")
+    .then(async (res) => {
+        const dataa = await res.json();
+        console.log("KIDS", dataa);
+        const pee=dataa.length;
+        console.log(pee);
+    
+
+
+const template = document.getElementById("section-template");
+const main = document.querySelector("main");
+
+// Populate and append sections
+dataa.forEach((data, index) => {
+
+    // Clone the template content
+    const section = template.content.cloneNode(true);
+
+    // Populate the template content with data
+    const img = section.querySelector("img");
+    img.src = data.download_url;
+    console.log(data.download_url)
+   
+    // Append the populated section to <main>
+    main.appendChild(section);
+});
+
+    })
+    .catch(err => console.error(err));
